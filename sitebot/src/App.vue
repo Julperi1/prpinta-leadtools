0f214dd3098d468d855d21d06eaf709793927583
Julius Paananen <137898167+Julperi1@users.noreply.github.com>
2025-03-20 16:47:50 +0200

    init sitebot

---
+++ sitebot/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { computed, provide } from 'vue';
+import SiteBot from '@/components/SiteBot.vue';
+import { useDisplay } from 'vuetify';
+
+const mobile = computed(() => {
+  return useDisplay().smAndDown;
+});
+
+provide('mobile', mobile);
+</script>
+<template>
+  <v-app>
+    <v-main>
+      <SiteBot />
+    </v-main>
+  </v-app>
+</template>
---
+++ sitebot/src/components/SiteBot.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref, inject } from 'vue';
+
+const mobile = inject('mobile');
+
+const open = ref({
+  minimized: true,
+  small: false,
+  full: false,
+});
+
+</script>
+
+<template>
+  <v-bottom-sheet v-model="open.minimized" :scrim="false" persistent no-click-animation class="mx-2 elevation-0"
+    rounded="lg">
+    <v-card>
+      <v-sheet rounded height="5" class="mt-1 mx-2 mb-2 bg-grey-lighten-2"></v-sheet>
+    </v-card>
+
+  </v-bottom-sheet>
+
+  <v-bottom-sheet v-model="open.small" width="300" rounded :scrim="false" persistent no-click-animation>
+    <v-card>
+      wqe
+    </v-card>
+  </v-bottom-sheet>
+
+  <v-bottom-sheet v-model="open.full" width="300" :scrim="false" persistent no-click-animation>
+    <v-card>
+      wqeqweqweqwe
+    </v-card>
+  </v-bottom-sheet>
+</template>
+<style scoped>
+* {
+  box-shadow: none !important;
+  background-color: red !important;
+}
+</style>
